cb11e2a3594eca01a4021c8c23d67a893a6a2d66
adamgibbons <adam.d.gibbons@gmail.com>
2014-09-04 12:32:48 -0700

    adds angular directive tutorial demo

---
+++ tutorials/angular-directive-part-1/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('app', []);
+
+app.controller('ContactsCtrl', function($scope) {
+  $scope.moveliners = [
+    {name: 'Brittany', title: 'Move Captain', img_title: 'brittany-seaton'},
+    {name: 'Russell', title: 'Move Navigator', img_title: 'russell-matney'},
+    {name: 'Adam', title: 'Driver, Foreman', img_title: 'adam-gibbons'}
+  ];
+});
+
+app.directive('mvlnContacts', function() {
+  return {
+    restrict: 'EA',
+    templateUrl: './moveliner.html',
+    scope: {
+      contacts: '='
+    },
+    link: function(scope, element, attributes) {
+      scope.contact = scope.contacts[0];
+
+      element.find('button').bind('click', function() {
+        if (scope.contact == scope.contacts[scope.contacts.length - 1]) {
+          scope.$apply(function() { scope.contact = scope.contacts[0]; });
+        } else {
+          scope.$apply(function() {
+            scope.contact = scope.contacts[scope.contacts.indexOf(scope.contact) + 1];
+          });
+        }
+      });
+    }
+  }
+});
